83c11be68a6d99df2416ce3232aae25980fed45d
karthikprabhu04 <kpp32@cam.ac.uk>
2025-08-27 23:15:06 +0100

    Added server-side validation for error-checking

--- routes/new.js
+++ routes/new.js
@@ -1,16 +1,29 @@
 const { Router } = require("express");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
 
 const newRouter = Router();
+
+const validateUser = [
+  body("name").trim()
+    .isAlpha().withMessage("Name must only contain letters"),
+  body("text").trim()
+    .isAlpha().withMessage("Text must only contain letters"),
+];
 
 newRouter.get("/", (req, res) => {
   res.render("form");
 });
 
-newRouter.post("/", (req, res) => {
-  console.log(req.body);
+newRouter.post("/", [validateUser, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("form", {
+      errors: errors.array(),
+    })
+  }
   db.addMessage({ text: req.body.text, user: req.body.name, added: new Date() });
-  res.redirect("/")
-})
+  res.redirect("/")}]
+);
 
 module.exports = newRouter;
